4f82e99d9896d7a6c50ca3c8755794789351cac0
Gaetan LAVIT <g.lavit@outlook.fr>
2022-10-06 10:01:44 +0400

    correction du middleware de vérification de token

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    const token = req.header('auth-token') || req.cookies('token');
+    const token = req.header('auth-token') || req.cookies.token;
     if (!token) res.status(401).send('Accès refusé')
     try {
         const verified = jwt.verify(token, process.env.SECRET_TOKEN)
