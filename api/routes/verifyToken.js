5b2ed04cf5ce71b86cbd5e82590f1797a361e967
Gaetan LAVIT <g.lavit@outlook.fr>
2022-10-15 15:02:40 +0400

    restriction du token repondant

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
     const token = req.header('auth-token') || req.cookies.token;
     if (!token) res.status(401).send('Accès refusé')
+    if(JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()).role === 'repondant') return res.status(403).send({message: 'Role insuffisant'})
     try {
         const verified = jwt.verify(token, process.env.SECRET_TOKEN)
         // accessible depuis les routes
